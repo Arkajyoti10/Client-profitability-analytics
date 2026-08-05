8718311be925c037b93f26e9a959305866119d12
Arkajyoti Basu Ray <140254975+Arkajyoti10@users.noreply.github.com>
2026-08-06 01:40:53 +0530

    Add files via upload

---
+++ SQL queries/tables/client.sql
@@ -0,0 +1,11 @@
+CREATE TABLE clients(
+	client_id VARCHAR(250),
+	client_name VARCHAR(250),
+	industry VARCHAR(250),
+	relationship_start_year INT,
+	region VARCHAR(250),
+	tenure_years INT
+);
+
+SELECT *
+FROM clients;
